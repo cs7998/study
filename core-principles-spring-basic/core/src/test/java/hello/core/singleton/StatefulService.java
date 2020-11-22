c05bd3ed0165f59abbb867edcc07c6925c89d957
unho.shin <cs7998@gmail.com>
2020-11-22 14:18:43 +0900

    [Spring Basic] convert stateless small Example

--- core-principles-spring-basic/core/src/test/java/hello/core/singleton/StatefulService.java
+++ core-principles-spring-basic/core/src/test/java/hello/core/singleton/StatefulService.java
@@ -3,14 +3,19 @@ package hello.core.singleton;
 public class StatefulService {
     // Ctrl + Shift + T  Create Test
 
-    private int price;  // 상태를 유지하는 필드
+//    private int price;  // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+//    public void order(String name, int price) {
+//        System.out.println("name + " + name + " price = "+ price);
+//        this.price = price; // 여기가 문제!
+//    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+
+    public int order(String name, int price) {
         System.out.println("name + " + name + " price = "+ price);
-        this.price = price; // 여기가 문제!
-    }
-
-    public int getPrice() {
         return price;
     }
 }
--- core-principles-spring-basic/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core-principles-spring-basic/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,17 +16,20 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA : A사용자 10000원 주문
-        statefulService1.order("UserA", 10000);
+        int userAPrice = statefulService1.order("UserA", 10000);
         // ThreadB : B사용자 20000원 주문
-        statefulService2.order("UserB", 20000);
+        int userBPrice = statefulService2.order("UserB", 20000);
 
         // ThreadA : A사용자 주문금액 조회
-        int price = statefulService1.getPrice();
+        //int price = statefulService1.getPrice();
+
 
         // A의 주문 금액이 20000 으로 조회 된다.
         // 같은 인스턴스 객체이므로 statefulService1, statefulService2 이름은 다른지만만
-       System.out.println("price = " + price);
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        //System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
+
+        //assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
