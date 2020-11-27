fbc1b25f230f0aa2dcc5d84b8a196a8416a23421
unho.shin <cs7998@gmail.com>
2020-11-27 10:28:09 +0900

    [Spring Basic] Lombok example

---
+++ core-principles-spring-basic/core/src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+// lombok 사용법 찾아보기
+// https://projectlombok.org/features/all
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("Angora");
+
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
--- core-principles-spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core-principles-spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,11 +3,13 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // @Component("service") conflictingBeanDefinitionException
 @Component
+@RequiredArgsConstructor // 롬북 자바의 final 이 붙은 필드를 모아서 자바의 annotation processor 기능 으로 컴파일 시점에 생성자 자동 생성
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository; // = new MemoryMemberRepository();
@@ -15,11 +17,13 @@ public class OrderServiceImpl implements OrderService {
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // Ctrl + F12 클래스의 메소드 목록 팝업
+    // @RequiredArgsConstructor 이 생성자를 자동 생성하므로 중복 코드가 된다.
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
