33a5b3ae0cbc946b1a1e7c01ce48a3a9cb6f557a
unho.shin <cs7998@gmail.com>
2020-11-30 15:48:34 +0900

    [Spring Basic] Web Scope Exam with Error

---
+++ core-principles-spring-basic/core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;    // request scope 고객 요청 시점에 생성하므로 어플 시작 시점에는 없음 > Provider 를 사용해서 해결할수 있음.
+//    Caused by: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread; consider defining a scoped proxy for this bean if you intend to refer to it from a singleton; nested exception is java.lang.IllegalStateException: No thread-bound request found: Are you referring to request attributes outside of an actual web request, or processing a request outside of the originally receiving thread? If you are actually operating within a web request and still receive this message, your code is probably running outside of DispatcherServlet: In this case, use RequestContextListener or RequestContextFilter to expose the current request.
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+
+}
