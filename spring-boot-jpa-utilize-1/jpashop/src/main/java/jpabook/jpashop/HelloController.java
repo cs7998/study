4f9ac4e81190ef01ea27bf7e453fa2e90a965c4e
unho.shin <cs7998@gmail.com>
2020-12-01 18:12:12 +0900

    [JPA Utilize 1] Setting View Environment

---
+++ spring-boot-jpa-utilize-1/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "angora!");
+        return "hello";  // resources/templates 하위에 hello.html 파일에 연결
+    }
+}
